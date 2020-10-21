18e9225a3d4ccf5b1ddb4e15f4c86fa6fd7a3652
Nozcepni <53128374+Nozcepni@users.noreply.github.com>
2020-10-21 12:42:36 +0300

    backend eklendi

---
+++ cardealer/src/main/java/com/cardealer/cardealer/CardealerApplication.java
@@ -0,0 +1,14 @@
+package com.cardealer.cardealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardealerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardealerApplication.class, args);
+		System.out.println("fdf");
+	}
+
+}
